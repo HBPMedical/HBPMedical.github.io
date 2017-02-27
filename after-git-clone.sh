51689da7b37e3535a8c80d86707492a462ac3491
Ludovic Claude <ludovic.claude@laposte.net>
2017-02-28 00:32:43 +0100

    Improve after-git-clone

--- after-git-clone.sh
+++ after-git-clone.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 git fetch origin content
-git worktree add ../HBPMedical.github.io.content content
+[ -d ../HBPMedical.github.io.content ] || git worktree add ../HBPMedical.github.io.content content
 
 cd ../HBPMedical.github.io.content
 ./after-git-clone.sh
