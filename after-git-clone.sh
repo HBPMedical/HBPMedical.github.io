7e8471affd2b3167749186dfa9f8186ee9b130f1
elia-s <elia_s@outlook.it>
2017-06-19 11:35:40 +0200

    Moved old content to master-old, uploaded latest documentation from fbf

--- after-git-clone.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -e
-
-git fetch origin content
-[ -d ../HBPMedical.github.io.content ] || git worktree add ../HBPMedical.github.io.content origin/content
-
-cd ../HBPMedical.github.io.content
-git checkout content
-./after-git-clone.sh
-cd -
